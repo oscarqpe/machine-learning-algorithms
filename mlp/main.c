89fa7a4502fd6d1c2a6619b5c22ac6fbbbca8ce6
=Oscar Edmit Quispe Poccohuanca <oscarqc@nspsac.com>
2016-04-11 12:41:21 -0500

    second commmt

--- mlp/main.c
+++ mlp/main.c
@@ -7,11 +7,18 @@
 int main()
 {
     srand(time(NULL));
+    int totalRows = 150;
+    int sizeOfInput = 4;
+    int neuronByLayer = 4;
+    int hiddenLayers = 2;
+    int neuronByOutput = 1;
+    double factorLearning = 0.5;
+
 
     char buffer[1024] ;
     char *record,*line;
     int ii=0,jj=0;
-    double data[610][59];
+    double data[totalRows][sizeOfInput + 1];
     FILE *fstream = fopen("\iris.data.csv","r");
     if(fstream == NULL)
     {
@@ -27,17 +34,16 @@ int main()
 			tok = strtok(NULL, ";\n"))
         {
             printf("Field  %s\n", tok);
+            data[ii][jj] = atof(tok);
+            printf("I  %d\n", ii);
+            jj++;
         }
-
+        ii++;
         // NOTE strtok clobbers tmp
         free(tmp);
     }
 
-    int sizeOfInput = 4;
-    int neuronByLayer = 4;
-    int hiddenLayers = 2;
-    int neuronByOutput = 1;
-    double factorLearning = 0.5;
+
     // array to input
     double input[sizeOfInput];
 
@@ -48,117 +54,123 @@ int main()
     double output[neuronByOutput][sizeOfInput + 3];
 
     // input data
-    input[0] = 2;
-    input[1] = 3;
-    input[2] = 5;
+    //input[0] = 2;
+    //input[1] = 3;
+    //input[2] = 5;
 
-    int h, i,j,k;
+    int h, i,j,k, l;
 
-    /********************FORWARD PROPAGATION***********************/
-    // forward to first layer
-    for (i = 0; i < neuronByLayer; i++) {
-        for (j = 0; j < sizeOfInput; j++) {
-            //printf("%lf", layer1[i][j]);
-            //printf(" ");
-            if (layer1[0][i][j] <= 0.01) {
-                //printf("i");
-                layer1[0][i][j] = 0.01+(rand() % 1);
-            }
-            layer1[0][i][sizeOfInput] += input[j] * layer1[0][i][j];
+    for (l = 0; l < totalRows; l++) {
+        // copy each row of data to input
+        for (i = 0; i < sizeOfInput; i++) {
+            input[i] = data[l][i];
+            printf("%fl", input[i]);
         }
-        layer1[0][i][sizeOfInput + 1] = (1 / ( 1 + exp((double) -1 * layer1[0][i][sizeOfInput])));
-        layer1[0][i][sizeOfInput + 2] = 0.0;
-    }
-
-    // forward to hidden layers
-    for (h = 1; h < hiddenLayers; h++) {
+        /********************FORWARD PROPAGATION***********************/
+        // forward to first layer
         for (i = 0; i < neuronByLayer; i++) {
             for (j = 0; j < sizeOfInput; j++) {
                 //printf("%lf", layer1[i][j]);
                 //printf(" ");
-                if (layer1[h][i][j] <= 0.01) {
+                if (layer1[0][i][j] <= 0.01) {
                     //printf("i");
-                    layer1[h][i][j] = 0.01+(rand() % 1);
+                    layer1[0][i][j] = 0.01+(rand() % 1);
                 }
-                layer1[h][i][sizeOfInput] += layer1[h - 1][i][sizeOfInput + 1] * layer1[h][i][j];
+                layer1[0][i][sizeOfInput] += input[j] * layer1[0][i][j];
             }
-            layer1[h][i][sizeOfInput + 1] = (1 / ( 1 + exp((double) -1 * layer1[0][i][sizeOfInput])));
-            layer1[h][i][sizeOfInput + 2] = 0.0;
+            layer1[0][i][sizeOfInput + 1] = (1 / ( 1 + exp((double) -1 * layer1[0][i][sizeOfInput])));
+            layer1[0][i][sizeOfInput + 2] = 0.0;
         }
-    }
-    // fordward to outout layer
-    for (i = 0; i < neuronByOutput; i++) {
-        for (j = 0; j < sizeOfInput; j++) {
-            if (output[i][j] <= 0.01) {
-                //printf("i");
-                output[i][j] = 0.01 + (rand() % 1);
-            }
-            output[i][sizeOfInput] += output[i][j] * layer1[hiddenLayers - 1][neuronByLayer - 1][j];
-        }
-        output[i][sizeOfInput + 1] = (1 / ( 1 + exp((double) -1 * output[i][sizeOfInput])));
-        output[i][sizeOfInput + 2] = 0.0;
-    }
 
-    /*********************BACKWARD PROPAGATION********************/
-    // calc delta error output layer
-    for (i = 0; i < neuronByOutput; i++) {
-        // error de la capa de salida que funcion utilizar ??????
-        output[i][sizeOfInput + 2] = output[i][sizeOfInput + 1] - output[i][sizeOfInput];
-    }
-    // calc delta error from last hidden layers
-    for (i = 0; i < neuronByLayer; i++) {
-        for (j = 0; j < sizeOfInput; j++) {
-            double sum = 0;
-            for (k = 0; k < neuronByOutput; k++) {
-                sum += output[k][sizeOfInput + 2] * output[k][j];
+        // forward to hidden layers
+        for (h = 1; h < hiddenLayers; h++) {
+            for (i = 0; i < neuronByLayer; i++) {
+                for (j = 0; j < sizeOfInput; j++) {
+                    //printf("%lf", layer1[i][j]);
+                    //printf(" ");
+                    if (layer1[h][i][j] <= 0.01) {
+                        //printf("i");
+                        layer1[h][i][j] = 0.01+(rand() % 1);
+                    }
+                    layer1[h][i][sizeOfInput] += layer1[h - 1][i][sizeOfInput + 1] * layer1[h][i][j];
+                }
+                layer1[h][i][sizeOfInput + 1] = (1 / ( 1 + exp((double) -1 * layer1[0][i][sizeOfInput])));
+                layer1[h][i][sizeOfInput + 2] = 0.0;
             }
-            layer1[hiddenLayers - 1][i][sizeOfInput + 2] = sum;
         }
-    }
-    // calc delta error to hidden layers
-    for (h = hiddenLayers - 2; h >= 0; h--) {
+        // fordward to outout layer
+        for (i = 0; i < neuronByOutput; i++) {
+            for (j = 0; j < sizeOfInput; j++) {
+                if (output[i][j] <= 0.01) {
+                    //printf("i");
+                    output[i][j] = 0.01 + (rand() % 1);
+                }
+                output[i][sizeOfInput] += output[i][j] * layer1[hiddenLayers - 1][neuronByLayer - 1][j];
+            }
+            output[i][sizeOfInput + 1] = (1 / ( 1 + exp((double) -1 * output[i][sizeOfInput])));
+            output[i][sizeOfInput + 2] = 0.0;
+        }
+
+        /*********************BACKWARD PROPAGATION********************/
+        // calc delta error output layer
+        for (i = 0; i < neuronByOutput; i++) {
+            // error de la capa de salida que funcion utilizar ??????
+            output[i][sizeOfInput + 2] = output[i][sizeOfInput + 1] - output[i][sizeOfInput];
+        }
+        // calc delta error from last hidden layers
         for (i = 0; i < neuronByLayer; i++) {
             for (j = 0; j < sizeOfInput; j++) {
                 double sum = 0;
-                for (k = 0; k < neuronByLayer; k++) {
-                    sum += layer1[h + 1][k][sizeOfInput + 2] * layer1[h + 1][k][j];
+                for (k = 0; k < neuronByOutput; k++) {
+                    sum += output[k][sizeOfInput + 2] * output[k][j];
                 }
-                layer1[h][i][sizeOfInput + 2] = sum;
+                layer1[hiddenLayers - 1][i][sizeOfInput + 2] = sum;
             }
         }
-    }
-
-    // UPDATE WEIGHTS
-    // update weight first layer
-    for (i = 0; i < sizeOfInput; i++) {
-        for (j = 0; j < neuronByLayer; j++) {
-            layer1[0][i][j] = layer1[0][i][j]
-                            + factorLearning * layer1[0][i][sizeOfInput + 2]
-                            * (layer1[0][i][sizeOfInput] * (1 - layer1[0][i][sizeOfInput]))
-                            * input[i];
+        // calc delta error to hidden layers
+        for (h = hiddenLayers - 2; h >= 0; h--) {
+            for (i = 0; i < neuronByLayer; i++) {
+                for (j = 0; j < sizeOfInput; j++) {
+                    double sum = 0;
+                    for (k = 0; k < neuronByLayer; k++) {
+                        sum += layer1[h + 1][k][sizeOfInput + 2] * layer1[h + 1][k][j];
+                    }
+                    layer1[h][i][sizeOfInput + 2] = sum;
+                }
+            }
         }
-    }
 
-    // update weight hidden layers
-    for (h = 1; h < hiddenLayers; h++) {
+        // UPDATE WEIGHTS
+        // update weight first layer
         for (i = 0; i < sizeOfInput; i++) {
             for (j = 0; j < neuronByLayer; j++) {
-                layer1[h][i][j] = layer1[h][i][j]
-                                + factorLearning * layer1[h][i][sizeOfInput + 2]
-                                * (layer1[h][i][sizeOfInput] * (1 - layer1[h][i][sizeOfInput]))
-                                * layer1[h - 1][i][sizeOfInput];
+                layer1[0][i][j] = layer1[0][i][j]
+                                + factorLearning * layer1[0][i][sizeOfInput + 2]
+                                * (layer1[0][i][sizeOfInput] * (1 - layer1[0][i][sizeOfInput]))
+                                * input[i];
+            }
+        }
+
+        // update weight hidden layers
+        for (h = 1; h < hiddenLayers; h++) {
+            for (i = 0; i < sizeOfInput; i++) {
+                for (j = 0; j < neuronByLayer; j++) {
+                    layer1[h][i][j] = layer1[h][i][j]
+                                    + factorLearning * layer1[h][i][sizeOfInput + 2]
+                                    * (layer1[h][i][sizeOfInput] * (1 - layer1[h][i][sizeOfInput]))
+                                    * layer1[h - 1][i][sizeOfInput];
+                }
+            }
+        }
+
+        // update weight ouput layer
+        for (i = 0; i < neuronByLayer; i++) {
+            for (j = 0; j < neuronByOutput; j++) {
+                output[i][j] = output[i][j] + factorLearning * output[i][sizeOfInput + 2]
+                                * (output[i][sizeOfInput] * (1 - output[i][sizeOfInput])) * layer1[hiddenLayers - 1][i][sizeOfInput];
             }
         }
     }
-
-    // update weight ouput layer
-    for (i = 0; i < neuronByLayer; i++) {
-        for (j = 0; j < neuronByOutput; j++) {
-            output[i][j] = output[i][j] + factorLearning * output[i][sizeOfInput + 2]
-                            * (output[i][sizeOfInput] * (1 - output[i][sizeOfInput])) * layer1[hiddenLayers - 1][i][sizeOfInput];
-        }
-    }
-
     /**********************PRINT NETWORK NEURON**********************************/
     printf("\n");
 
